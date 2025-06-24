d9a368ac90b956fa82bf3bd85c313ccc984c849a
HanaPark1 <ydjxlst@gmail.com>
2025-06-24 13:13:53 +0900

    feat: photocard model 수정

--- popin/photocard/models.py
+++ popin/photocard/models.py
@@ -4,10 +4,16 @@ from datetime import date
 
 class TempUser(models.Model):
     user_id = models.CharField(max_length=50)
+    
+    def __str__(self):
+        return f'{self.user_id}'
 
 class TempIdol(models.Model):
     groub = models.CharField(max_length=50)
     member = models.CharField(max_length=50)
+    
+    def __str__(self):
+        return f'{self.groub} | {self.member}'
         
 class Photocard(models.Model):
     CATEGORY_CHOICES = [
@@ -49,7 +55,7 @@ class Photocard(models.Model):
 
     pno = models.AutoField(primary_key=True)
     seller = models.ForeignKey(TempUser, on_delete=models.CASCADE, related_name='sell')
-    buyer = models.ForeignKey(TempUser, on_delete=models.SET_NULL, null=True, related_name='buy')
+    buyer = models.ForeignKey(TempUser, on_delete=models.SET_NULL, null=True, blank=True, related_name='buy')
 
     title = models.CharField(max_length=100, default='title')
     image = models.ImageField(upload_to='photocards/')
@@ -58,12 +64,12 @@ class Photocard(models.Model):
     idol = models.ForeignKey(TempIdol, on_delete=models.CASCADE, related_name='photocards')
 
     poca_state = models.CharField(max_length=20, choices=P_STATE_CHOICES, default='상')
-    tag = models.CharField(max_length=20, null=True, blank=False)  # choices 생략됨
+    tag = models.CharField(max_length=20, null=True, blank=True)  # choices 생략됨
     trade_type = models.CharField(max_length=20, choices=TRADE_CHOICES, null=True)
     place = models.CharField(max_length=20, choices=PLACE_CHOICES, null=True)
 
     sell_state = models.CharField(max_length=20, choices=TRADE_STATE_CHOICES, default='전')
-    buy_state = models.CharField(max_length=20, choices=TRADE_STATE_CHOICES, null=True)
+    buy_state = models.CharField(max_length=20, choices=TRADE_STATE_CHOICES, null=True, blank=True)
 
     hit = models.IntegerField(default=0)
     created_at = models.DateTimeField(auto_now_add=True)
@@ -73,4 +79,4 @@ class Photocard(models.Model):
     longitude = models.FloatField(null=True, blank=True)
 
     def __str__(self):
-        return f'{self.title} ({self.group}/{self.member})'
+        return f'{self.title} ({self.idol.groub}/{self.idol.member})'
