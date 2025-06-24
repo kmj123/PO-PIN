01e12597ebacc9a431cf85a5b4e4610a96ee0fea
HanaPark1 <ydjxlst@gmail.com>
2025-06-24 15:37:27 +0900

    feat: urls.py main branch 반영

--- popin/popin/urls.py
+++ popin/popin/urls.py
@@ -5,6 +5,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('landing.urls')),
+    path('mypage/', include('mypage.urls')),
     path('photocard/', include('photocard.urls')),
 ]
 
