5519204eaa4339ff39b93ee00520a66ee68de4a9
HanaPark1 <ydjxlst@gmail.com>
2025-06-27 14:07:22 +0900

    fix: 사용자 == 작성자일 시 수정

--- popin/photocard/views.py
+++ popin/photocard/views.py
@@ -122,55 +122,74 @@ def write(request):
 
 # 포토카드 거래글 수정
 def update(request, pno):
-    # 포토카드 상세정보 불러오기 (context 작성 참고)
-    photo_qs = Photocard.objects.get(pno=pno)
-    if request.method == "GET":
-        
-        member_qs = Member.objects.all()
-        context = {
-            'category_choices': Photocard.CATEGORY_CHOICES,
-            'poca_state_choices': Photocard.P_STATE_CHOICES,
-            'trade_type_choices': Photocard.TRADE_CHOICES,
-            'place_choices': Photocard.PLACE_CHOICES,
-            'trade_state_choices' : Photocard.TRADE_STATE_CHOICES,
-            'member': member_qs,
-            'photocard': photo_qs
-        }
-        return render(request, 'update.html', context)
+    user_id = request.session.get('user_id')  # 로그인 시 저장한 user_id 세션
     
-    # 포토카드 상세정보 수정
-    elif request.method == "POST":
-        photo_qs.title = request.POST.get('title')
-        photo_qs.image = request.FILES.get('image')
-        
-        photo_qs.category=request.POST.get('category')
-        photo_qs.album=request.POST.get('album')
-        
-        member_id=request.POST.get('member')
-        member_obj = Member.objects.get(pk=int(member_id))
-        photo_qs.member = member_obj
-        
-        photo_qs.poca_state=request.POST.get('poca_state')
-        photo_qs.tag=request.POST.get('tag', None)
-        
-        photo_qs.trade_type=request.POST.get('trade_type')
-        photo_qs.place=request.POST.get('place')
-        
-        photo_qs.sell_state = request.POST.get('sell_state')
-        
-        if request.POST.get('available_at') == "" :
-            available_at = str(date.today())
-        else:
-            available_at = request.POST.get('available_at')
-        
-        photo_qs.available_at = available_at
-        
-        photo_qs.latitude=request.POST.get('latitude')
-        photo_qs.longitude=request.POST.get('longitude')
-        
-        photo_qs.save()
-        
+    if not user_id:
+        return redirect('login:loginp')  # 로그인 안 되어있으면 로그인 페이지로
+    
+    user = User.objects.get(user_id=user_id) # 사용자
+    photo_qs = Photocard.objects.get(pno=pno) # 수정 포토카드 게시글
+    
+    # 사용자 아이디와 수정 포토카드 게시글의 판매자가 같을 시 True
+    if user.user_id == photo_qs.seller.user_id :
+        try:
+            if request.method == "GET":
+                member_qs = Member.objects.all() # DB 내 아이돌 전체
+                context = {
+                    'category_choices': Photocard.CATEGORY_CHOICES,
+                    'poca_state_choices': Photocard.P_STATE_CHOICES,
+                    'trade_type_choices': Photocard.TRADE_CHOICES,
+                    'place_choices': Photocard.PLACE_CHOICES,
+                    'trade_state_choices' : Photocard.TRADE_STATE_CHOICES,
+                    'member': member_qs,
+                    'photocard': photo_qs
+                }
+                return render(request, 'update.html', context)
+            
+            # 포토카드 상세정보 수정
+            elif request.method == "POST":
+                photo_qs.title = request.POST.get('title') # 제목
+                photo_qs.image = request.FILES.get('image') # 이미지
+                
+                photo_qs.category=request.POST.get('category') # 카테고리 (공방, 앨범)
+                photo_qs.album=request.POST.get('album') # 활동 시기 앨범 (1집, 2집)
+                
+                member_id=request.POST.get('member') # 아이돌
+                member_obj = Member.objects.get(pk=int(member_id))
+                photo_qs.member = member_obj
+                
+                photo_qs.poca_state=request.POST.get('poca_state') # 포카 하자 상태
+                photo_qs.tag=request.POST.get('tag', None) # 태그
+                
+                photo_qs.trade_type=request.POST.get('trade_type') # 거래 방식
+                photo_qs.place=request.POST.get('place') # 장소 (올공, 더현대)
+                
+                photo_qs.sell_state = request.POST.get('sell_state') # 판매자 거래 상태
+                
+                # 거래 가능일
+                if request.POST.get('available_at') == "" :
+                    available_at = str(date.today())
+                else:
+                    available_at = request.POST.get('available_at')
+                
+                photo_qs.available_at = available_at 
+                
+                #거래 위치 위도 경도
+                photo_qs.latitude=request.POST.get('latitude')
+                photo_qs.longitude=request.POST.get('longitude')
+                
+                # 새로 설정한 값 수정
+                photo_qs.save()
+                
+                # 수정 완료 후 리다이렉트
+                return redirect('/photocard/list')
+        except User.DoesNotExist:
+            return redirect('login:main')  # 예외 상황 대비
+    else: 
+        # 사용자 아이디와 판매자 아이디가 일치하지 않을 경우 리다이렉트
         return redirect('/photocard/list')
+                
+        
 
 # 포토카드 거래글 삭제
 def delete(request, pno):
