274bc2f23a4e76e8a5a2ad48ab274ad558fb1cda
HanaPark1 <ydjxlst@gmail.com>
2025-07-01 09:50:30 +0900

    fix: write_sharing name 수정

--- popin/community/views.py
+++ popin/community/views.py
@@ -3,5 +3,5 @@ from django.shortcuts import render
 def write_review(request):
     return render(request, 'community_write_review.html')
 
-def write_review(request):
+def write_sharing(request):
     return render(request, 'community_write_sharing.html')
