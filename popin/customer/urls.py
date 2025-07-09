1fe2d7db95e7e22005d117375efbc00a6ae0ae3a
HanaPark1 <ydjxlst@gmail.com>
2025-07-09 20:11:48 +0900

    feat: notice list, view 반환

--- popin/customer/urls.py
+++ popin/customer/urls.py
@@ -5,6 +5,7 @@ app_name = 'customer'
 urlpatterns = [
     path('information/', views.information, name='information'),
     path('notice/', views.notice, name='notice'),
-    path('notice_view/', views.notice_view, name='notice_view'),
+    path('notice_view/<int:id>/', views.notice_view, name='notice_view'),
     path('qna/', views.QnA, name='QnA'),
+    path('filter_notice/', views.filter_notice, name='filter_notice'),
 ]
