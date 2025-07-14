b48e5a1b91f4d132696425bba312a205205f1677
HanaPark1 <ydjxlst@gmail.com>
2025-07-14 09:45:21 +0900

    feat: 채팅룸 생성

--- popin/chatting/admin.py
+++ popin/chatting/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from chatting.models import ChatMessage, ChatRoom
 
 # Register your models here.
+
+admin.site.register(ChatMessage)
+admin.site.register(ChatRoom)
--- popin/chatting/urls.py
+++ popin/chatting/urls.py
@@ -2,7 +2,10 @@ from django.contrib import admin
 from django.urls import path, include
 from . import views
 
-app_name = 'change'
+app_name = 'chatting'
 urlpatterns = [
-    path('', views.signupFT, name='signupFT'),
+    path('', views.chatting, name='chatting'),
+    path("test/",views.test,name="test"),
+    path("start_chat/",views.start_chat,name="start_chat"),
+    # path("room/<str:room_name>/",views.room,name="room"),
 ]
